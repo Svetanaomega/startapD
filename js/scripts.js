eb0f61072431888ea41ed83996f042d41a16b873
svetanaomega <svetanaomega@gmail.com>
2019-01-12 20:25:27 +0400

    finelly version

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,21 @@
 
-$('#carouselExample').on('slide.bs.carousel', function (e) {
-  
-  var $e = $(e.relatedTarget);
-  var idx = $e.index();
-  var itemsPerSlide = 4;
-  var totalItems = $('.carousel-item').length;
-  
-  if (idx >= totalItems-(itemsPerSlide-1)) {
-      var it = itemsPerSlide - (totalItems - idx);
-      for (var i=0; i<it; i++) {
-          // append slides to end
-          if (e.direction=="left") {
-              $('.carousel-item').eq(i).appendTo('.carousel-inner');
-          }
-          else {
-              $('.carousel-item').eq(0).appendTo('.carousel-inner');
-          }
-      }
-  }
-});
-
-$('#carouselExample').carousel({ 
-              interval: 2000
-      });
-
- $(document).ready(function() { 
-/* show lightbox when clicking a thumbnail */
-  $('a.thumb').click(function(event){
-    event.preventDefault();
-    var content = $('.modal-body');
-    content.empty();       var title = $(this).attr("title");
-      $('.modal-title').html(title);               content.html($(this).html());
-      $(".modal-profile").modal({show:true});
-  });
-
-});
-
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        450:{
+            items:2,
+        },
+        600:{
+            items:3,
+            loop:false
+        },
+        1000:{
+            items:4,
+        }
+    }
+})
